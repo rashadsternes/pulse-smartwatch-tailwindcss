78d625ce6af220bf80e659db3f07b1885cfba19b
rashadsternes <nrsternes@gmail.com>
2025-06-21 05:13:45 -0500

    Render Navbar w/ Logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
+import Navbar from './components/Navbar.jsx'
 
 
 function App() {
 
   return (
     <div>
+      <Navbar />
       <h1 className="text-3xl font-bold underline">Pulse Smartwatch</h1>
     </div>
   )
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import logo from "../assets/pulse-smartwatch-logo.png"
+
+const Navbar = () => {
+  return (
+    <nav className="sticky top-0 z-50 py-3 backdrop-blur-lg border-b
+    border-neutral-700/80">
+      <div className="container px-4 mx-auto relative text-sm">
+        <div className="flex justify-center items-center">
+          <div className="flex items-center flex-shrink-0">
+            <img className="h-10 w-10 mr-2" src={logo} alt="logo" />
+            <span className="text-xl tracking-tight">Pulse</span>
+          </div>
+        </div>
+      </div>
+    </nav>
+
+  )
+}
+
+export default Navbar;
