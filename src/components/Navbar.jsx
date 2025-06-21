bf387e456d28eafadad25339857ba86b2e4ce2bc
rashadsternes <nrsternes@gmail.com>
2025-06-21 06:30:18 -0500

    Add Mobile Drawer

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,14 @@
+import { useState } from "react";
+import { Menu, X } from "lucide-react";
 import logo from "../assets/pulse-smartwatch-logo.png";
 import { navItems } from "../constants";
 
 const Navbar = () => {
+  const [ mobileDrawerOpen, setMobileDrawerOpen ] = useState(false);
+  const toggleNavbar = () => {
+    setMobileDrawerOpen(!mobileDrawerOpen);
+  };
+
   return (
     <nav className="sticky top-0 z-50 py-3 backdrop-blur-lg border-b
     border-neutral-700/80">
@@ -22,7 +29,28 @@ const Navbar = () => {
             <a href="#" className="py-2 px-3 border rounded-md">Sign In</a>
             <a href="#" className="bg-gradient-to-r from-purple-500 to-purple-800 py-2 px-3 rounded-md">Create an Account</a>
           </div>
+          <div className="lg:hidden md:flex flex-col justify-end">
+            <button onClick={toggleNavbar}>
+              { mobileDrawerOpen ? <X /> : <Menu /> }
+            </button>
+          </div>
         </div>
+        { mobileDrawerOpen && (
+          <div className="fixed right-0 z-20 bg-neutral-900 w-full p-12 
+          flex flex-col justify-center items-center lg:hidden">
+            <ul>
+              {navItems.map((item, ind) => (
+              <li key={ind} className="py-4">
+                <a href={item.href}>{item.label}</a>
+              </li>
+            ))}
+            </ul>
+            <div className="flex space-x-6 ml-10">
+              <a href="#" className="py-2 px-6 w-25 border rounded-md ">Sign In</a>
+              <a href="#" className="py-2 px-4 w-37 bg-gradient-to-r from-purple-500 to-purple-800 border rounded-md ">Create Account</a>
+            </div>
+          </div>
+        )}
       </div>
     </nav>
 
