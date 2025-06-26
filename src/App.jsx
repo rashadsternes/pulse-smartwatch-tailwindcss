a91e6e3c540f295eda7c60d50f2e7a74af616bea
rashadsternes <nrsternes@gmail.com>
2025-06-26 12:42:25 -0500

    Build out Feature Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Navbar from './components/Navbar.jsx';
 import HeroSection from './components/Hero.jsx';
-
+import FeatureSection from './components/Features.jsx';
 
 function App() {
 
@@ -9,6 +9,7 @@ function App() {
       <Navbar />
       <div className="max-w-7xl mx-auto pt=20 px-6">
         <HeroSection />
+        <FeatureSection />
       </div>
         
     </div>
