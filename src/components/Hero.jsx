fd8635ce4d615a781822ee504e9a01d555379009
rashadsternes <nrsternes@gmail.com>
2025-06-26 11:38:49 -0500

    Reformat document

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,10 +11,7 @@ const HeroSection = () => {
         Pulse is the screenless performance wearable that monitors your heart rate, sleep, recovery, and strain—so you can optimize every move you make.
       </p>
       <div className="flex justify-center my-10">
-        <a
-          href="#"
-          className="bg-gradient-to-r from-purple-500 to-purple-800 py-3 px-4 mx-3 rounded-md"
-        >
+        <a href="#" className="bg-gradient-to-r from-purple-500 to-purple-800 py-3 px-4 mx-3 rounded-md">
           Start Free Trial 
         </a>
         <a href="#" className="py-3 px-4 mx-3 rounded-md border">
@@ -22,20 +19,10 @@ const HeroSection = () => {
         </a>
       </div>
       <div className="flex mt-10 justify-center">
-        <video
-          autoPlay
-          loop
-          muted
-          className="rounded-lg w-1/2 border border-purple-700 shadow-sm shadow-purple-400 mx-2 my-4"
-        >
+        <video autoPlay loop muted className="rounded-lg w-1/2 border border-purple-700 shadow-sm shadow-purple-400 mx-2 my-4">
           <source src={smartwatchVideo} type="video/mp4" />
         </video>
-        <video
-          autoPlay
-          loop
-          muted
-          className="rounded-lg w-1/2 border border-purple-700 shadow-sm shadow-purple-400 mx-2 my-4"
-        >
+        <video autoPlay loop muted className="rounded-lg w-1/2 border border-purple-700 shadow-sm shadow-purple-400 mx-2 my-4">
           <source src={manUsingSmartwatch} type="video/mp4" />
         </video>
         {/* // Stock footage provided by <a class="link author-link-popup" target="_blank" href="https://www.videvo.net/author/freepik/#rs=attribution-modal">Freepik</a>, downloaded from <a class="videvo-redirect" target="_blank" href="https://www.videvo.net/#rs=attribution-modal">videvo.net</a> */}
